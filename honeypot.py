5773905325d34d3f7a2b68570f86320c7a3c76a8
Kaio <boxdzpr0@gmail.com>
2025-08-08 21:43:03 +1000

    finalise all the code without comment

---
+++ honeypot.py
@@ -0,0 +1,43 @@
+# Libraries
+import argparse
+from ssh_honeypot import *
+from web_honeypot import *
+
+# Parse Arguments
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-a','--address', type=str, required=True)
+    parser.add_argument('-p','--port',type=int,required=True)
+    parser.add_argument('-u','--username',type=str)
+    parser.add_argument('-pw','--password',type=str)
+
+    parser.add_argument('-s','--ssh',action="store_true")
+    parser.add_argument('-w','--http', action="strore_true")
+
+    args = parser.parse_args()
+
+    try:
+        if args.ssh:
+            print("[-] Running SSH Honeypot...")
+            honeypot(args.address, args.port, args.username, args.password)
+
+            if not args.username:
+                username = None
+            if not args.password:
+                password = None
+        elif args.http:
+            print("[-] Running HTTP WordPress Honeypot...")
+
+            if not args.username:
+                args.username = "admin"
+            if not args.password:
+                args.password = "password"
+
+            print(f"Port: {args.port} Username: {args.username} Password {args.password}")
+            run_web_honeypot(args.port, args.username, args.password)
+        else:
+            print("[!] Choose a honeypot type (SSH --ssh) or (HTTP --http).")
+    except :
+        print("\n Exiting HONEYPY...\n")
